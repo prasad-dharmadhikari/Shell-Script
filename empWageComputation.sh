376ab61dcbf0b43b9085180a0faa4aaaae632083
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-02-25 10:33:44 +0530

    ADDED:Employee attendence feature to check whether employee is present or not

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,3 +1,12 @@
 #!bin/bash -x
 
 echo "Welcome to employee wage computation"
+isPresent=1
+randomCheck=$((RANDOM%2))
+
+if [ $isPresent -eq $randomCheck ]
+then
+	echo "Employee is present"
+else
+	echo "Employee is absent"
+fi
