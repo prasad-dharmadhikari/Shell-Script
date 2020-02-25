51880c508ae919f11ea1807cfea624d5692e9494
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-02-25 11:07:20 +0530

    ADDED:Calculate daily employee wage feature to calculate daily wage of employee

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -11,3 +11,12 @@ then
 else
 	echo "Employee is absent"
 fi
+# To calculate daily employee wage
+if [ $isPresent -eq $randomCheck ]
+then
+	empRatePerHr=20
+	empHrs=8
+	salary=$(($empRatePerHr*$empHrs))
+else
+	salary=0
+fi
