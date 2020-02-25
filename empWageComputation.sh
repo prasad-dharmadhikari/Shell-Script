9e83aeecd20b914d6414fbf31c455caeb0087bbd
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-02-25 12:38:04 +0530

    ADDED:Part time employee and it's wage feature

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,7 +2,11 @@
 
 echo "Welcome to employee wage computation"
 isPresent=1
-randomCheck=$((RANDOM%2))
+isPartTime=1
+isFullTime=2
+empRatePerHr=20
+
+randomCheck=$((RANDOM%3))
 
 # To check whether employee is present or not
 if [ $isPresent -eq $randomCheck ]
@@ -14,9 +18,20 @@ fi
 # To calculate daily employee wage
 if [ $isPresent -eq $randomCheck ]
 then
-	empRatePerHr=20
 	empHrs=8
 	salary=$(($empRatePerHr*$empHrs))
 else
 	salary=0
 fi
+# To add part time employee and calculate it's wage
+if [ $isPartTime -eq $randomCheck ]
+then
+	empHrs=4
+elif [ $isFullTime -eq $randomCheck ]
+then
+	empHrs=8
+else
+	empHrs=0
+fi
+salary=$(($empRatePerHr*$empHrs))
+
