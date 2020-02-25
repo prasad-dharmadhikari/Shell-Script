ec066a0a14c4f72b5fbd16228084b6e224aaa9b9
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-02-25 13:03:46 +0530

    ADDED:Add part time employee and calculate it's salary using case statement

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -23,15 +23,19 @@ then
 else
 	salary=0
 fi
-# To add part time employee and calculate it's wage
-if [ $isPartTime -eq $randomCheck ]
-then
-	empHrs=4
-elif [ $isFullTime -eq $randomCheck ]
-then
-	empHrs=8
-else
-	empHrs=0
-fi
+# To add part time employee and calculate it's wage using case statement
+case $randomCheck in
+	$isPartTime)
+				empHrs=4
+				;;
+	$isFullTime)
+				empHrs=8
+				;;
+				*)
+				empHrs=0
+				;;
+esac
+
 salary=$(($empRatePerHr*$empHrs))
+
 
