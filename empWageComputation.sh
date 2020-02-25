7f7a403c6e5a1e52acf483ab6eb146568142661c
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-02-25 14:33:14 +0530

    ADDED:Feature for calculating the wage for a month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -5,18 +5,20 @@ isPresent=1
 isPartTime=1
 isFullTime=2
 empRatePerHr=20
+numWorkingDays=20
+totalSalary=0
 
-randomCheck=$((RANDOM%3))
+employeeCheck=$((RANDOM%3))
 
 # To check whether employee is present or not
-if [ $isPresent -eq $randomCheck ]
+if [ $isPresent -eq $employeeCheck ]
 then
 	echo "Employee is present"
 else
 	echo "Employee is absent"
 fi
 # To calculate daily employee wage
-if [ $isPresent -eq $randomCheck ]
+if [ $isPresent -eq $employeeCheck ]
 then
 	empHrs=8
 	salary=$(($empRatePerHr*$empHrs))
@@ -24,17 +26,23 @@ else
 	salary=0
 fi
 # To add part time employee and calculate it's wage using case statement
-case $randomCheck in
-	$isPartTime)
-		empHrs=4
-		;;
-	$isFullTime)
-		empHrs=8
-		;;
-	*)
-		empHrs=0
-		;;
-esac
-salary=$(($empRatePerHr*$empHrs))
+#To calculate the wage for month
+for((day=1;day<=$numWorkingDays;day++))
+do
+	employeeCheck=$((RANDOM%3))
+	case $employeeCheck in
+		$isPartTime)
+			empHrs=4
+			;;
+		$isFullTime)
+			empHrs=8
+			;;
+		*)
+			empHrs=0
+			;;
+	esac
+	salary=$(($empRatePerHr*$empHrs))
+	totalSalary=$(($totalSalary+$salary))
+done
 
 
